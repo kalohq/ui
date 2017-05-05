4b686599d2dd4b459251dbbcbbce359536bc1e7e
Alex Pate <alex.pate@lystable.com>
2017-05-05 18:03:38 +0100

    Change dir structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,21 @@
-export {Button} from './button';
-export {ButtonGroup} from './button-group';
-export {FieldHint} from './field-hint';
-export {H1} from './h1';
-export {H3} from './h3';
-export {Heading} from './heading';
-export {Icon} from './icon';
-export {Input} from './input';
-export {List} from './list';
-export {LoadingSpinner} from './loading-spinner';
-export {Radio} from './radio';
-export {StarRating} from './star-rating';
-export {StepMarks} from './step-marks';
-export {Text} from './text';
-export {ToggleButton} from './toggle-button';
+/**
+ * Typography
+ */
+export { H1 } from './typography/h1';
+export { H2 } from './typography/h2';
+export { H3 } from './typography/h3';
+export { H4 } from './typography/h4';
+export { Text } from './typography/text';
+export { Heading } from './typography/heading';
+
+export { Button } from './button';
+export { ButtonGroup } from './button-group';
+export { FieldHint } from './field-hint';
+export { Icon } from './icon';
+export { Input } from './input';
+export { List } from './list';
+export { LoadingSpinner } from './loading-spinner';
+export { Radio } from './radio';
+export { StarRating } from './star-rating';
+export { StepMarks } from './step-marks';
+export { ToggleButton } from './toggle-button';
