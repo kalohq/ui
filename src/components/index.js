06c61d696b8cdde121bce657a3f8cf61f3b00f04
Alex Pate <alex.pate@lystable.com>
2017-05-12 14:38:33 +0100

    Temporarily exclude some components from published module

--- src/components/index.js
+++ src/components/index.js
@@ -1,22 +1,22 @@
 /**
   * Typography
 **/
-export {default as H1} from './h1';
-export {default as H2} from './h2';
-export {default as H3} from './h3';
-export {default as H4} from './h4';
-export {default as Text} from './text';
-export {default as Heading} from './heading';
+// export {default as H1} from './h1';
+// export {default as H2} from './h2';
+// export {default as H3} from './h3';
+// export {default as H4} from './h4';
+// export {default as Text} from './text';
+// export {default as Heading} from './heading';
 
 export {default as Button} from './button';
-export {default as ButtonGroup} from './button-group';
-export {default as FieldHint} from './field-hint';
+// export {default as ButtonGroup} from './button-group';
+// export {default as FieldHint} from './field-hint';
 export {default as Icon} from './icon';
-export {default as Input} from './input';
-export {default as List} from './list';
+// export {default as Input} from './input';
+// export {default as List} from './list';
 export {default as LoadingSpinner} from './loading-spinner';
-export {default as Radio} from './radio';
-export {default as StarRating} from './star-rating';
-export {default as StepMarks} from './step-marks';
-export {default as ToggleButton} from './toggle-button';
-export {default as PrimaryActionButton} from './primary-action-button';
+// export {default as Radio} from './radio';
+// export {default as StarRating} from './star-rating';
+// export {default as StepMarks} from './step-marks';
+// export {default as ToggleButton} from './toggle-button';
+// export {default as PrimaryActionButton} from './primary-action-button';
