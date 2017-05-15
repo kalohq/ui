c61a4790eed328cccac0ed0b2712f6d9a8e1c7b7
Alex Pate <alex.pate@lystable.com>
2017-05-15 18:23:49 +0100

    Import relevant tests

--- src/components/avatar/__tests__/avatar.test.js
+++ src/components/avatar/__tests__/avatar.test.js
@@ -5,6 +5,9 @@ import {shallow} from 'enzyme';
 import {testComponent} from 'utils/test/react';
 
 import Avatar, {AvatarImage} from '../avatar';
+import CustomIcon from 'components/custom-icon';
+import Icon from 'components/icon';
+import LoadingSpinner from 'components/loading-spinner';
 
 const _src = '//wherever.com/record.png';
 const _subRecordSrc = '//wherever.com/subrecord.png';
@@ -35,8 +38,59 @@ describe('components/avatar', () => {
       expect(
         result.find(AvatarImage).props().src
       ).toBe(_src);
-      expect(result.find(Avatar).length).toBe(1);
-      expect(result.find(Avatar).props().record).toBe(props.subRecord);
+      expect(
+        result.find(Avatar).length
+      ).toBe(1);
+      expect(
+        result.find(Avatar).props().record
+      ).toBe(props.subRecord);
+    });
+
+    it('should render a lysted badge if confirmed', () => {
+      const {element} = create({
+        confirmed: true,
+        size: 'small',
+      });
+      const result = shallow(element);
+      expect(
+        result.find(CustomIcon).filter({children: 'lysted'}).length
+      ).toBe(1);
+      result.setProps({size: 'large'});
+      expect(
+        result.find(CustomIcon).filter({children: 'lysted'}).length
+      ).toBe(1);
+    });
+
+    it('should render upload prompts in upload mode', () => {
+      const {element} = create({
+        editable: true,
+      });
+      const result = shallow(element);
+      expect(
+        result.find(Icon).filter({children: 'edit'}).length
+      ).toBe(1);
+    });
+
+    it('should render upload prompts once a new image has been uploaded', () => {
+      const {element} = create({
+        showUploaded: true,
+        uploaded: true,
+      });
+      const result = shallow(element);
+      expect(
+        result.find(Icon).filter({children: 'done'}).length
+      ).toBe(1);
+    });
+
+    it('should render an uploading prompt in uploading mode', () => {
+      const {element} = create({
+        editable: true,
+        uploading: true,
+      });
+      const result = shallow(element);
+      expect(
+        result.find(LoadingSpinner).length
+      ).toBe(1);
     });
 
   });
