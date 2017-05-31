bd486c7b0de555011cf17921d530ea348f4d0e50
Alex Pate <alex.pate@lystable.com>
2017-05-31 13:28:52 +0100

    Update to use newer storybook package name

--- src/components/avatar/__stories__/avatar.stories.js
+++ src/components/avatar/__stories__/avatar.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {storiesOf} from '@kadira/storybook';
+import {storiesOf} from '@storybook/react';
 
 import Avatar from '../avatar';
 
@@ -8,40 +8,46 @@ storiesOf('Avatar', module)
     'Standard avatar',
     "Renders a standard avatar with a users initials as fallback if the image src doesn't exist.",
     () => {
-      return <Avatar record="Remy Sharp" size="medium" src="https://s3.amazonaws.com/uifaces/faces/twitter/rem/128.jpg" />;
-    }
+      return (
+        <Avatar
+          record="Remy Sharp"
+          size="medium"
+          src="https://s3.amazonaws.com/uifaces/faces/twitter/rem/128.jpg"
+        />
+      );
+    },
   )
   .addWithInfo(
     'with loading state',
-    "With a loading spinner. Displayed if the avatar is uploading",
+    'With a loading spinner. Displayed if the avatar is uploading',
     () => {
       return <Avatar record="Remy Sharp" size="medium" uploading={true} />;
-    }
+    },
   )
   .addWithInfo(
     'with uploaded complete state',
-    "Displayed if uploading is complete",
+    'Displayed if uploading is complete',
     () => {
       return <Avatar record="Remy Sharp" size="medium" uploaded={true} />;
-    }
+    },
   )
   .addWithInfo(
     'editable',
-    "Useful to display whether an avatar is editable or not.",
+    'Useful to display whether an avatar is editable or not.',
     () => {
       return <Avatar record="Remy Sharp" size="large" editable={true} />;
-    }
+    },
   )
   .addWithInfo(
     'confirmed',
     "Displays the lysted icon to show whether a user has 'claimed' their profile",
     () => {
       return <Avatar record="Lauren Plews" confirmed={true} />;
-    }
+    },
   )
   .addWithInfo(
     'with subrecord',
-    "Avatars can be nested to display a secondary avatar (for example, a company)",
+    'Avatars can be nested to display a secondary avatar (for example, a company)',
     () => {
       return (
         <Avatar
@@ -51,5 +57,5 @@ storiesOf('Avatar', module)
           subRecord="Harry Roberts"
         />
       );
-    }
+    },
   );
