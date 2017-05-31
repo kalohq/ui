a096a710c297b4667e2f3b8277dae3ae10ceb600
Alex Pate <alex.pate@lystable.com>
2017-05-31 13:34:23 +0100

    Fix incorrect package import name

--- src/components/custom-icon/__stories__/custom-icon.stories.js
+++ src/components/custom-icon/__stories__/custom-icon.stories.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import {storiesOf} from '@kadira/storybook';
+import {storiesOf} from '@storybook/react';
 
 import CustomIcon from 'components/custom-icon';
 
-storiesOf('CustomIcon', module)
-  .addWithInfo(
-    'An icon',
-    'A component to display custom icons',
-    () => {
-      return (
-        <span>
-          <CustomIcon size={120}>lysted</CustomIcon>
-          <CustomIcon size={120}>payment</CustomIcon>
-          <CustomIcon size={120}>team</CustomIcon>
-          <CustomIcon size={120}>invoice</CustomIcon>
-          <CustomIcon size={120}>get-lysted</CustomIcon>
-          <CustomIcon size={120}>careers</CustomIcon>
-        </span>
-      );
-    }
+storiesOf(
+  'CustomIcon',
+  module,
+).addWithInfo('An icon', 'A component to display custom icons', () => {
+  return (
+    <span>
+      <CustomIcon size={120}>lysted</CustomIcon>
+      <CustomIcon size={120}>payment</CustomIcon>
+      <CustomIcon size={120}>team</CustomIcon>
+      <CustomIcon size={120}>invoice</CustomIcon>
+      <CustomIcon size={120}>get-lysted</CustomIcon>
+      <CustomIcon size={120}>careers</CustomIcon>
+    </span>
   );
+});
