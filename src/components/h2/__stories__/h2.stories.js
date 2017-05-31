e22c69fd5d52629a0ea6daeb02b80a7923e5660f
Alex Pate <alex.pate@lystable.com>
2017-05-31 11:52:40 +0100

    Update to Storybook 3

--- src/components/h2/__stories__/h2.stories.js
+++ src/components/h2/__stories__/h2.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {storiesOf} from '@kadira/storybook';
+import {storiesOf} from '@storybook/react';
 
 import H2 from 'components/h2';
 
--- src/components/h3/__stories__/h3.stories.js
+++ src/components/h3/__stories__/h3.stories.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {storiesOf} from '@kadira/storybook';
+import {storiesOf} from '@storybook/react';
 
 import H3 from 'components/h3';
 
 storiesOf(
   'Typography',
-  module
+  module,
 ).addWithInfo('H3', 'This is our default heading weight/size', () => {
   return <H3>Project Documents & Notes</H3>;
 });
