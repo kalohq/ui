2a3cb2341593155f0f40f83c2428b3abfab01cb3
Alex Pate <alex@kalohq.com>
2017-06-02 13:26:46 +0100

    Expose new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,3 @@
-/**
-  * Typography
-**/
 // export {default as H1} from './h1';
 // export {default as H2} from './h2';
 // export {default as H3} from './h3';
@@ -9,14 +6,7 @@
 // export {default as Heading} from './heading';
 
 export {default as Button} from './button';
-// export {default as ButtonGroup} from './button-group';
-// export {default as FieldHint} from './field-hint';
+export {default as Checkbox} from './checkbox';
 export {default as Icon} from './icon';
-// export {default as Input} from './input';
-// export {default as List} from './list';
 export {default as LoadingSpinner} from './loading-spinner';
-// export {default as Radio} from './radio';
-// export {default as StarRating} from './star-rating';
-// export {default as StepMarks} from './step-marks';
-// export {default as ToggleButton} from './toggle-button';
-// export {default as PrimaryActionButton} from './primary-action-button';
+export {default as Radio} from './radio';
