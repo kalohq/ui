03fefe9c3e660514dfd8ac8e32282eeb6baf23b0
Alex Pate <alex@kalohq.com>
2017-06-02 10:55:14 +0100

    Refactor heading components

--- src/components/heading/heading.js
+++ src/components/heading/heading.js
@@ -1,124 +1,97 @@
+/* @flow */
 import React from 'react';
-import PropTypes from 'prop-types';
 import cx from 'classnames';
 import {parseStyleProps} from 'utils/style';
-
-import PureComponent from 'react-pure-render/component';
 import Icon from '../icon';
 
 import styles from './heading.css';
 
-const ICON_SIZE = {
-  small: '14',
-  medium: '14',
-  large: '14',
-  'extra-large': '24',
+import type {
+  HEADING_NUMBER,
+  HEADING_WEIGHT,
+  HEADING_SIZE,
+  HEADING_MARGIN,
+  HEADING_COLOR,
+  HEADING_ALIGN,
+  HEADING_HOVER,
+} from './constants';
+
+type headingProps = {
+  children: React$Element<*>,
+  number: HEADING_NUMBER,
+  weight: HEADING_WEIGHT,
+  size: HEADING_SIZE,
+  margin: HEADING_MARGIN,
+  color: HEADING_COLOR,
+  flex: boolean,
+  border: boolean,
+  multiline: boolean,
+  align: HEADING_ALIGN,
+  hover: HEADING_HOVER,
+  icon: string,
+  iconAfter: boolean,
+  iconPadding: number,
 };
 
-export default class Heading extends PureComponent {
-  static propTypes = {
-    number: PropTypes.oneOf(['1', '2', '3', '4']),
-    weight: PropTypes.oneOf(['normal', 'semi-bold']),
-    size: PropTypes.oneOf([
-      'extra-extra-small',
-      'extra-small',
-      'small',
-      'medium',
-      'large',
-      'extra-large',
-    ]),
-    margin: PropTypes.oneOf(['none', 'tiny', 'small', 'medium', 'large']),
-    color: PropTypes.oneOf([
-      'red',
-      'blue',
-      'dark-grey',
-      'light-grey',
-      'white',
-      'grey',
-    ]),
-    align: PropTypes.oneOf(['center', 'left', 'right', 'none']),
-    hover: PropTypes.oneOf(['underline', 'none']),
-    flex: PropTypes.bool,
-    multiline: PropTypes.bool,
-    border: PropTypes.bool,
-    children: PropTypes.node,
-    icon: PropTypes.string,
-    iconAfter: PropTypes.string,
-    iconPadding: PropTypes.number,
-  };
+export default function Heading(props: headingProps) {
+  const {
+    children,
+    number = 3,
+    weight = 'normal',
+    size = 'small',
+    margin = 'none',
+    color = 'dark-grey',
+    flex = false,
+    border = false,
+    multiline = false,
+    align = 'none',
+    hover = 'none',
+    icon = false,
+    iconAfter = false,
+    iconPadding = 30,
+    ...otherProps
+  } = props;
 
-  static defaultProps = {
-    number: '3',
-    weight: 'normal',
-    size: 'small',
-    color: 'dark-grey',
-    margin: 'none',
-    flex: false,
-    border: false,
-    multiline: false,
-    align: 'none',
-    hover: 'none',
-    icon: '',
-    iconAfter: '',
-    iconPadding: 10,
-  };
+  const DOMElement = `h${number}`;
+  const propStyle = otherProps.style;
+  const {unstyledProps, style} = parseStyleProps(otherProps);
 
-  render() {
-    const {
-      number,
-      weight,
-      size,
-      color,
-      margin,
-      children,
-      multiline,
-      border,
-      flex,
-      icon,
-      iconAfter,
-      iconPadding,
-      align,
-      hover,
-      ...otherProps
-    } = this.props;
-
-    const DOMHeading = `h${number}`;
-    const propStyle = otherProps.style;
-    const {props, style} = parseStyleProps(otherProps);
-
-    return (
-      <DOMHeading
-        className={cx({
-          [styles.root]: true,
-          [styles[`weight-${weight}`]]: true,
-          [styles[`size-${size}`]]: true,
-          [styles[`color-${color}`]]: true,
-          [styles[`margin-${margin}`]]: true,
-          [styles[`align-${align}`]]: true,
-          [styles[`hover-${hover}`]]: true,
-          [styles.multiline]: multiline,
-          [styles.flex]: flex,
-          [styles.border]: border,
-        })}
-        {...props}
-        style={{...style, ...propStyle}}
-      >
-        {icon
-          ? <Icon
-              size={ICON_SIZE[size]}
-              color="grey"
-              paddingRight={iconPadding}
-            >
-              {icon}
-            </Icon>
-          : null}
-        {children}
-        {iconAfter
-          ? <Icon size={ICON_SIZE[size]} color="grey" paddingLeft={iconPadding}>
-              {iconAfter}
-            </Icon>
-          : null}
-      </DOMHeading>
-    );
-  }
+  return (
+    <DOMElement
+      className={cx({
+        [styles.root]: true,
+        [styles[`weight-${weight}`]]: true,
+        [styles[`size-${size}`]]: true,
+        [styles[`color-${color}`]]: true,
+        [styles[`margin-${margin}`]]: true,
+        [styles[`align-${align}`]]: true,
+        [styles[`hover-${hover}`]]: true,
+        [styles.multiline]: multiline,
+        [styles.flex]: flex,
+        [styles.border]: border,
+      })}
+      style={{...style, ...propStyle}}
+      {...unstyledProps}
+    >
+      {icon
+        ? <Icon
+            size={size === 'extra-large' ? 24 : 14}
+            color={color}
+            paddingRight={iconPadding}
+          >
+            {icon}
+          </Icon>
+        : null}
+      {children}
+      {iconAfter
+        ? <Icon
+            size={size === 'extra-large' ? 24 : 14}
+            color="grey"
+            paddingLeft={iconPadding}
+          >
+            {iconAfter}
+          </Icon>
+        : null}
+    </DOMElement>
+  );
 }
