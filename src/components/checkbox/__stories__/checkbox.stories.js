f12a9aeb120a226b49d6bdfd59a21b37d20fd56f
Alex Pate <alex@kalohq.com>
2017-06-02 11:33:57 +0100

    Update to use new package name

--- src/components/checkbox/__stories__/checkbox.stories.js
+++ src/components/checkbox/__stories__/checkbox.stories.js
@@ -1,32 +1,49 @@
 import React from 'react';
-import {storiesOf} from '@kadira/storybook';
+import {storiesOf} from '@storybook/react';
 
 import Checkbox from '../checkbox';
 
 storiesOf('Checkbox', module)
   .addWithInfo(
     'checkbox',
-    "A standard checkbox. Pass a label using the prop to ensure that interaction happens on both the checkbox and the label",
+    'A standard checkbox. Pass a label using the prop to ensure that interaction happens on both the checkbox and the label',
     () => {
-      return <Checkbox label="Onboarding freelancers" size="medium" checked={false} />;
-    }
+      return (
+        <Checkbox
+          label="Onboarding freelancers"
+          size="medium"
+          checked={false}
+        />
+      );
+    },
   )
   .addWithInfo(
     'readonly',
-    "A non-interactive checkbox. Can still displayed a checked state",
+    'A non-interactive checkbox. Can still displayed a checked state',
     () => {
-      return <Checkbox label="Onboarding freelancers" size="medium" checked={true} readonly={true} />;
-    }
+      return (
+        <Checkbox
+          label="Onboarding freelancers"
+          size="medium"
+          checked={true}
+          readonly={true}
+        />
+      );
+    },
   )
   .addWithInfo(
     'sizing',
-    "Checkboxes can be in two sizes. Medium and Large",
+    'Checkboxes can be in two sizes. Medium and Large',
     () => {
       return (
         <span>
-          <Checkbox label="Onboarding freelancers" size="medium" checked={false} />
+          <Checkbox
+            label="Onboarding freelancers"
+            size="medium"
+            checked={false}
+          />
           <Checkbox label="Freelancer invoices" size="large" checked={false} />
         </span>
       );
-    }
+    },
   );
