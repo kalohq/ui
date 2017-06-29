4fa3c7ce81a3a090cadfa430074ca6c2892a79d9
Alex Pate <alex@kalohq.com>
2017-06-29 14:39:43 +0100

    Add new stories for disabled/readonly radio

--- src/components/radio/__stories__/radio.stories.js
+++ src/components/radio/__stories__/radio.stories.js
@@ -8,16 +8,56 @@ const myClickFunction = () => {
   window.alert('Hello from an onClick event');
 };
 
-storiesOf(
-  'Radio',
-  module,
-).addWithInfo('Single Radio', 'A standard radio button', () => {
-  return (
-    <Radio
-      label="Turn off notifications"
-      size="medium"
-      checked={false}
-      onClick={myClickFunction}
-    />
-  );
-});
+storiesOf('Radio', module)
+  .addWithInfo('Single Radio', 'A standard radio button', () => {
+    return (
+      <span>
+        <Radio
+          label="Turn off notifications"
+          checked={true}
+          onClick={myClickFunction}
+        />
+        <Radio
+          label="Enable email notifications"
+          checked={false}
+          onClick={myClickFunction}
+        />
+      </span>
+    );
+  })
+  .addWithInfo('disabled', 'A disabled radio button', () => {
+    return (
+      <span>
+        <Radio
+          label="Turn off notifications"
+          checked={true}
+          onClick={myClickFunction}
+          disabled={true}
+        />
+        <Radio
+          label="Enable email notifications"
+          checked={false}
+          onClick={myClickFunction}
+          disabled={true}
+        />
+      </span>
+    );
+  })
+  .addWithInfo('readonly', 'A readonly radio button', () => {
+    return (
+      <span>
+        <Radio
+          label="Turn off notifications"
+          checked={true}
+          onClick={myClickFunction}
+          readonly={true}
+        />
+        <Radio
+          label="Enable email notifications"
+          checked={false}
+          onClick={myClickFunction}
+          readonly={true}
+        />
+      </span>
+    );
+  });
