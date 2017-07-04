c72483fa08807fe0c89b7d9b47ac8422a5a6ea9e
Alex Pate <alex@kalohq.com>
2017-07-04 13:04:02 +0100

    Remove components that aren't actually part of the repo yet

--- src/components/input/__stories__/input.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {storiesOf} from '@storybook/react';
-
-import Input from 'components/input';
-
-storiesOf('Input', module).addWithInfo('Input', 'A standard text input', () => {
-  return <Input placeholder="Search freelancers…" size="medium" />;
-});
--- src/components/primary-action-button/index.js
+++
@@ -1,2 +0,0 @@
-/* @flow */
-export {default} from './primary-action-button';
