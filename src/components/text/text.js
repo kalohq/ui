d47dfd5ee07e488e1337e4858f2ff5f58ecd9786
Alex Pate <alex@kalohq.com>
2017-07-12 11:15:07 +0100

    Don't set not-interactive prop to false by default

--- src/components/text/text.js
+++ src/components/text/text.js
@@ -63,7 +63,7 @@ export default function Text(props: textProps) {
     resetTransform = false,
     align = DEFAULT_ALIGN,
     noUnderline = false,
-    interactive = false,
+    interactive,
     dangerouslySetInnerHTML,
     target,
     href,
