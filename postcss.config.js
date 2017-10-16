78758179818e3da9c46526af5d697739a940b131
Alex Pate <alex@kalohq.com>
2017-10-16 16:54:30 +0100

    Don't convert color variables for storybook dev

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,1 @@
-const postCSSVariables = require('postcss-css-variables');
-const globalCSSVariables = require('./config/global_css_variables.js');
-
-module.exports = {
-  plugins: [
-    postCSSVariables(globalCSSVariables),
-  ]
-}
+module.exports = {};
