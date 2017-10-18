2e383beecbd251d637bc4e2e8112d4b27ae3baa7
Alex Pate <alex@kalohq.com>
2017-10-18 18:30:15 +0100

    Include the icon defs file

--- .storybook/view/decorator-centered.js
+++ .storybook/view/decorator-centered.js
@@ -4,23 +4,35 @@ import Node from './node';
 import Text from 'components/text';
 import H1 from 'components/h1';
 import H3 from 'components/h3';
+import IconSymbols from 'components/icon-symbols';
 
-export default function (renderStory) {
+export default function(renderStory) {
   let story = renderStory();
 
   let meta = story.props;
   return (
-    <Flex flexDirection="column" padding={[50, 50]} style={{backgroundColor: '#fff'}}>
+    <Flex
+      flexDirection="column"
+      padding={[50, 50]}
+      style={{backgroundColor: '#fff'}}
+    >
+      <IconSymbols />
       <H1 size="extra-large">{meta.context.story}</H1>
       <Text multiline={true}>{meta.info}</Text>
-      <Inline style={{border: '1px solid #eceff1'}} padding={[20]} marginTop={20}>
+      <Inline
+        style={{border: '1px solid #eceff1'}}
+        padding={[20]}
+        marginTop={20}
+      >
         <H3 marginBottom={10}>Example</H3>
-        <Inline>
-          {story}
-        </Inline>
+        <Inline>{story}</Inline>
       </Inline>
 
-      <Inline style={{border: '1px solid #eceff1', backgroundColor: '#f9fafc'}} padding={[20]} marginTop={20}>
+      <Inline
+        style={{border: '1px solid #eceff1', backgroundColor: '#f9fafc'}}
+        padding={[20]}
+        marginTop={20}
+      >
         <H3 marginBottom={10}>Source Code</H3>
         <Inline>
           <pre>
