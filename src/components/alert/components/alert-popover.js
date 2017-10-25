c1ca2a71d74ecedc06b872d8f92ccba71515e308
Chris Pearce <hello@chrispearce.me>
2017-10-25 17:32:56 +0100

    Add transition animation to alert popovers

--- src/components/alert/components/alert-popover.js
+++ src/components/alert/components/alert-popover.js
@@ -1,6 +1,7 @@
 /* @flow */
 import React from 'react';
 import {Box} from '../../layout';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 type TProps = {
   children: React$Element<*>,
@@ -11,12 +12,28 @@ export default function AlertPopover(props: TProps) {
   const {children, topOffset = 0} = props;
 
   return (
-    <Box position="fixed" right={32} top={topOffset + 16} maxWidth={400}>
-      {React.Children.map(children, child =>
-        <Box marginBottom={8}>
-          {React.cloneElement(child, {justifyContent: 'left'})}
-        </Box>
+    <CSSTransitionGroup
+      transitionName="t-slide-from-right"
+      transitionAppear={true}
+      transitionAppearTimeout={300}
+      transitionEnterTimeout={300}
+      transitionLeaveTimeout={300}
+      component={Box}
+      position="fixed"
+      right={32}
+      top={topOffset + 16}
+      maxWidth={400}
+      alignItems="flex-end"
+    >
+      {React.Children.map(
+        children,
+        child =>
+          child
+            ? <Box marginBottom={8}>
+                {React.cloneElement(child, {justifyContent: 'left'})}
+              </Box>
+            : null
       )}
-    </Box>
+    </CSSTransitionGroup>
   );
 }
