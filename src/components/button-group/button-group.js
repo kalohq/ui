dbcaf6744c2ec921cd64ffa7d3cdcd1388f7d0b1
Alex Pate <alex@kalohq.com>
2017-11-06 15:00:30 +0000

    Import ButtonGroup component

---
+++ src/components/button-group/button-group.js
@@ -0,0 +1,60 @@
+/* @flow */
+import * as React from 'react';
+import cx from 'classnames';
+
+import {Flex} from '../layout';
+
+import styles from './button-group.css';
+
+/**
+ * A basic container for grouped buttons
+ * - Clones children to ensure Button components received a 'grouped' prop
+ */
+
+type Props = {
+  children: React.Node,
+  flex?: boolean,
+  bordered?: boolean,
+  wide?: boolean,
+  spacing?: boolean,
+  reverse?: boolean,
+};
+
+export default function ButtonGroup(props: Props) {
+  const {
+    children,
+    wide = true,
+    bordered,
+    flex,
+    spacing,
+    reverse,
+    ...otherProps
+  } = props;
+
+  return (
+    <Flex
+      alignItems="center"
+      className={cx({
+        [styles.wide]: wide,
+        [styles.bordered]: bordered,
+        [styles.reverse]: reverse,
+      })}
+      {...otherProps}
+    >
+      {children ? (
+        React.Children.map(
+          children,
+          el =>
+            el
+              ? React.cloneElement(el, {
+                  grouped: !spacing,
+                  spacing: !!spacing,
+                  reverse,
+                  flex,
+                })
+              : null
+        )
+      ) : null}
+    </Flex>
+  );
+}
