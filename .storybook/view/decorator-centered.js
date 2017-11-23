339ac523e113bb7c428516af3fb8924e5ef89705
Alex Pate <alex@kalohq.com>
2017-11-23 13:07:59 +0000

    Add ThemeProvider to storybook view

--- .storybook/view/decorator-centered.js
+++ .storybook/view/decorator-centered.js
@@ -1,35 +1,50 @@
 import React from 'react';
+import {ThemeProvider} from 'emotion-theming';
 import {Flex, Inline} from 'components/layout';
 import Node from './node';
 import Text from 'components/text';
 import H1 from 'components/h1';
 import H3 from 'components/h3';
 
-export default function (renderStory) {
+import theme from 'components/theme';
+
+export default function(renderStory) {
   let story = renderStory();
 
   let meta = story.props;
   return (
-    <Flex flexDirection="column" padding={[50, 50]} style={{backgroundColor: '#fff'}}>
-      <H1 size="extra-large">{meta.context.story}</H1>
-      <Text multiline={true}>{meta.info}</Text>
-      <Inline style={{border: '1px solid #eceff1'}} padding={[20]} marginTop={20}>
-        <H3 marginBottom={10}>Example</H3>
-        <Inline>
-          {story}
+    <ThemeProvider theme={theme}>
+      <Flex
+        flexDirection="column"
+        padding={[50, 50]}
+        style={{backgroundColor: '#fff'}}
+      >
+        <H1 size="extra-large">{meta.context.story}</H1>
+        <Text multiline={true}>{meta.info}</Text>
+        <Inline
+          style={{border: '1px solid #eceff1'}}
+          padding={[20]}
+          marginTop={20}
+        >
+          <H3 marginBottom={10}>Example</H3>
+          <Inline>{story}</Inline>
         </Inline>
-      </Inline>
 
-      <Inline style={{border: '1px solid #eceff1', backgroundColor: '#f9fafc'}} padding={[20]} marginTop={20}>
-        <H3 marginBottom={10}>Source Code</H3>
-        <Inline>
-          <pre>
-            {React.Children.map(story.props.children, (root, idx) => (
-              <Node key={idx} depth={0} node={root} />
-            ))}
-          </pre>
+        <Inline
+          style={{border: '1px solid #eceff1', backgroundColor: '#f9fafc'}}
+          padding={[20]}
+          marginTop={20}
+        >
+          <H3 marginBottom={10}>Source Code</H3>
+          <Inline>
+            <pre>
+              {React.Children.map(story.props.children, (root, idx) => (
+                <Node key={idx} depth={0} node={root} />
+              ))}
+            </pre>
+          </Inline>
         </Inline>
-      </Inline>
-    </Flex>
+      </Flex>
+    </ThemeProvider>
   );
 }
