d7a205c34018a5891509acdb30b6c79a6d1767db
Alex Pate <alex@kalohq.com>
2017-11-29 17:53:38 +0000

    Primitive as class to ensure ref gets bound

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,36 +1,37 @@
 /* @flow */
-import React from 'react';
+import * as React from 'react';
 import {parseStyleAndSpacingProps} from 'utils/style';
 import {cx} from 'emotion';
+import PureComponent from 'react-pure-render/component';
 
 import {Box as GridBox, Flex as GridFlex} from 'grid-emotion';
 
 function extendPrimitive(DefaultComponent, display, defaultStyle) {
-  return (initialProps: any) => {
-    const {
-      component: Component = DefaultComponent,
-      innerRef,
-      style: propStyle,
-      forceStyle,
-      className,
-      ...otherProps
-    } = initialProps;
+  return class Primitive extends PureComponent {
+    render() {
+      const {
+        component: Component = DefaultComponent,
+        style: propStyle,
+        forceStyle,
+        className,
+        ...otherProps
+      } = this.props;
 
-    const {props, spacingProps, style} = parseStyleAndSpacingProps({
-      ...defaultStyle,
-      ...otherProps,
-    });
+      const {props, spacingProps, style} = parseStyleAndSpacingProps({
+        ...defaultStyle,
+        ...otherProps,
+      });
 
-    return (
-      <Component
-        innerRef={innerRef}
-        className={cx(className)}
-        css={{display, ...style, ...propStyle}}
-        style={{...forceStyle}}
-        {...spacingProps}
-        {...props}
-      />
-    );
+      return (
+        <Component
+          className={cx(className)}
+          css={{display, ...style, ...propStyle}}
+          style={{...forceStyle}}
+          {...spacingProps}
+          {...props}
+        />
+      );
+    }
   };
 }
 
@@ -63,5 +64,6 @@ InlineFlex.displayName = 'InlineFlex';
  * @todo: All of the html primitives should follow here so our api is consistent to the very root...
  * - Eg. Button, Input, Select, Table etc.
  */
+
 export const A = extendPrimitive('a', 'inline-block');
 A.displayName = 'A';
