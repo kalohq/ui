8c225327114039426a49f8f051a21fa7948e7820
Alex Pate <alex@kalohq.com>
2017-12-06 17:37:44 +0000

    Add snapshot test for text

---
+++ src/components/text/__tests__/text.test.js
@@ -0,0 +1,40 @@
+/* @flow */
+/* eslint-env jest */
+import * as React from 'react';
+import {sheet} from 'emotion';
+import serializer from 'jest-glamor-react';
+import renderer from 'react-test-renderer';
+
+import Text from 'components/text';
+
+expect.addSnapshotSerializer(serializer(sheet));
+
+describe('Text', () => {
+  const defaultProps = {
+    size: 'medium',
+    color: 'charcoal',
+  };
+  const create = (props = {}) =>
+    renderer
+      .create(
+        <Text {...defaultProps} {...props}>
+          Hello World
+        </Text>
+      )
+      .toJSON();
+
+  test('should render shallow component ok', () => {
+    const element = create();
+    expect(element).toMatchSnapshot();
+  });
+
+  test('should render green text', () => {
+    const element = create({color: 'green'});
+    expect(element).toMatchSnapshot();
+  });
+
+  test('should render large text', () => {
+    const element = create({size: 'large'});
+    expect(element).toMatchSnapshot();
+  });
+});
