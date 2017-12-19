b5bc85eda788a15aa9afd0aa65ff4a88e510142e
Alex Pate <alex@kalohq.com>
2017-12-19 13:34:15 +0000

    Always wrap Layout components in emotion

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,8 +1,13 @@
 /* @flow */
 import React from 'react';
-import {parseStyleProps} from 'utils/style';
+import {
+  parseStyleProps,
+  cleanProps,
+  spacing,
+  filterStyleProps,
+} from 'utils/style';
 import PureComponent from 'react-pure-render/component';
-import cx from 'classnames';
+import styled, {cx} from 'react-emotion';
 
 function makePrimitive(name, DefaultComponent, display, defaultStyle) {
   class Primitive extends PureComponent {
@@ -21,12 +26,14 @@ function makePrimitive(name, DefaultComponent, display, defaultStyle) {
         ...otherProps,
       });
 
+      const cleanedProps = cleanProps(props);
+
       return (
         <Component
           ref={elRef}
           className={cx(className)}
           css={{...style}}
-          {...props}
+          {...cleanedProps}
           style={{...propStyle}}
         />
       );
@@ -37,28 +44,127 @@ function makePrimitive(name, DefaultComponent, display, defaultStyle) {
   return Primitive;
 }
 
-/**
+class BaseComponent extends PureComponent {
+  render() {
+    const {
+      component: Component = 'div',
+      elRef,
+      style: propStyle,
+      className,
+      ...otherProps
+    } = this.props;
+
+    const cleanedProps = cleanProps(otherProps);
+
+    return (
+      <Component
+        ref={elRef}
+        className={className ? cx(className) : null}
+        {...cleanedProps}
+        style={{...propStyle}}
+      />
+    );
+  }
+}
+
+class SpanBaseComponent extends PureComponent {
+  render() {
+    const {
+      component: Component = 'span',
+      elRef,
+      style: propStyle,
+      className,
+      ...otherProps
+    } = this.props;
+
+    const cleanedProps = cleanProps(otherProps);
+
+    return (
+      <Component
+        ref={elRef}
+        className={className ? cx(className) : null}
+        {...cleanedProps}
+        style={{...propStyle}}
+      />
+    );
+  }
+}
+
+class ABaseComponent extends PureComponent {
+  render() {
+    const {
+      component: Component = 'a',
+      elRef,
+      style: propStyle,
+      className,
+      ...otherProps
+    } = this.props;
+
+    const cleanedProps = cleanProps(otherProps);
+
+    return (
+      <Component
+        ref={elRef}
+        className={className ? cx(className) : null}
+        {...cleanedProps}
+        style={{...propStyle}}
+      />
+    );
+  }
+}
+/** 
  * Layout primitives
  */
-export const Box = makePrimitive('Box', 'div', 'flex', {
-  position: 'relative',
-  flexDirection: 'column',
-  alignItems: 'stretch',
-  flexShrink: '0',
-  alignContent: 'flex-start',
-  display: 'flex',
-});
 
-export const Flex = makePrimitive('Flex', 'div', 'flex');
-export const Block = makePrimitive('Block', 'div', 'block');
-export const Inline = makePrimitive('Inline', 'span', 'inline-block', {
-  verticalAlign: 'bottom',
-});
-export const InlineFlex = makePrimitive('InlineFlex', 'span', 'inline-flex');
+export const Box = styled(BaseComponent)`
+  position: relative;
+  flex-direction: column;
+  align-items: stretch;
+  flex-shrink: 0;
+  align-content: flex-start;
+  display: flex;
+  ${spacing};
+  ${filterStyleProps};
+`;
+Box.displayName = 'Box';
+
+export const Flex = styled(BaseComponent)`
+  display: flex;
+  ${spacing};
+  ${filterStyleProps};
+`;
+Flex.displayName = 'Flex';
+
+export const Block = styled(BaseComponent)`
+  display: block;
+  ${spacing};
+  ${filterStyleProps};
+`;
+Block.displayName = 'Block';
+
+export const Inline = styled(BaseComponent)`
+  display: inline-block;
+  vertical-align: bottom;
+  ${spacing};
+  ${filterStyleProps};
+`;
+Inline.displayName = 'Inline';
+
+export const InlineFlex = styled(SpanBaseComponent)`
+  display: inline-flex;
+  ${spacing};
+  ${filterStyleProps};
+`;
+InlineFlex.displayName = 'InlineFlex';
 
 /**
  * All of our html primitives should follow here so our api is consistent to the very root
  * Eg. Button, Input, Select, Table etc.
  */
-export const A = makePrimitive('H1', 'a', 'inline-block');
-export const I = makePrimitive('I', 'i', 'inline-block');
+
+export const A = styled(ABaseComponent)`
+  display: inline-flex;
+  ${spacing};
+  ${filterStyleProps};
+`;
+A.displayName = 'A';
