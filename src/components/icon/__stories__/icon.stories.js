059aab6d062dda4fecf9f279875e9aefe81378ff
Alex Pate <alex@kalohq.com>
2018-01-03 17:06:49 +0000

    Make Icon stories easier to consume

--- src/components/icon/__stories__/icon.stories.js
+++ src/components/icon/__stories__/icon.stories.js
@@ -1,10 +1,28 @@
 /* @flow */
 import React from 'react';
+import styled from 'react-emotion';
 import {storiesOf} from '@storybook/react';
 
-import Icon from '../icon';
+import Icon from 'components/icon';
+import Text from 'components/text';
 
 import {ICONS} from '../constants';
+
+const GridContainer = styled('div')`
+  display: grid;
+  grid-gap: 1px;
+  grid-template-columns: repeat(5, 20%);
+  background-color: ${props => props.theme.colors.grey300};
+`;
+
+const IconBlock = styled('div')`
+  display: flex;
+  min-width: 210px;
+  align-items: center;
+  flex-direction: column;
+  padding: 8px;
+  background-color: ${props => props.theme.colors.grey200};
+`;
 
 storiesOf(
   'Icon',
@@ -14,13 +32,16 @@ storiesOf(
   'Renders an icon from the Google Material Design icon set. See material.io for a list of icons',
   () => {
     return (
-      <span>
+      <GridContainer>
         {ICONS.values.map(icon => (
-          <Icon key={icon} color="blue400" size={36}>
-            {icon}
-          </Icon>
+          <IconBlock key={icon}>
+            <Icon color="blue400" size={24}>
+              {icon}
+            </Icon>
+            <Text>{icon}</Text>
+          </IconBlock>
         ))}
-      </span>
+      </GridContainer>
     );
   }
 );
