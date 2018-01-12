39edca627f4e3a90dac3da04390e6a416777e6ac
Alex Pate <alex@kalohq.com>
2018-01-12 14:40:54 +0000

    Flesh out icon documentation

--- src/components/icon/__stories__/icon.stories.js
+++ src/components/icon/__stories__/icon.stories.js
@@ -1,44 +1,53 @@
 /* @flow */
 import React from 'react';
-import styled from 'react-emotion';
 
 import Icon from '../icon';
-import Text from '../../text';
-
-import {ICONS} from '../constants';
-
-const GridContainer = styled('div')`
-  display: grid;
-  grid-gap: 1px;
-  grid-template-columns: repeat(5, 20%);
-  background-color: ${props => props.theme.colors.grey300};
-`;
-
-const IconBlock = styled('div')`
-  display: flex;
-  min-width: 210px;
-  align-items: center;
-  flex-direction: column;
-  padding: 8px;
-  background-color: ${props => props.theme.colors.grey200};
-`;
 
 export const examples = [
   {
     title: 'Icons',
     description:
-      'Renders an icon from the Google Material Design icon set. See material.io for a list of icons',
+      'Renders an icon from the Google Material Design icon set. See the Brand/Icons page for a full list of icons',
     render: () => (
-      <GridContainer>
-        {ICONS.values.map(icon => (
-          <IconBlock key={icon}>
-            <Icon color="blue400" size={24}>
-              {icon}
-            </Icon>
-            <Text>{icon}</Text>
-          </IconBlock>
-        ))}
-      </GridContainer>
+      <Icon color="blue400" size={24}>
+        arrow_drop_down_circle
+      </Icon>
+    ),
+  },
+  {
+    title: 'Icon sizing',
+    description: 'Icons can be a variety of sizing',
+    render: () => (
+      <span>
+        <Icon size={12}>attach_money</Icon>
+        <Icon size={14}>attach_money</Icon>
+        <Icon size={16}>attach_money</Icon>
+        <Icon size={18}>attach_money</Icon>
+        <Icon size={20}>attach_money</Icon>
+        <Icon size={24}>attach_money</Icon>
+        <Icon size={26}>attach_money</Icon>
+        <Icon size={36}>attach_money</Icon>
+        <Icon size={48}>attach_money</Icon>
+      </span>
+    ),
+  },
+  {
+    title: 'Icon colors',
+    description:
+      'Any color from our palette can be passed in via the color prop',
+    render: () => (
+      <span>
+        <Icon color="pink300">loop</Icon>
+        <Icon color="pink400">loop</Icon>
+        <Icon color="pink500">loop</Icon>
+        <Icon color="pink600">loop</Icon>
+        <Icon color="pink700">loop</Icon>
+        <Icon color="blue300">loop</Icon>
+        <Icon color="blue400">loop</Icon>
+        <Icon color="blue500">loop</Icon>
+        <Icon color="blue600">loop</Icon>
+        <Icon color="blue700">loop</Icon>
+      </span>
     ),
   },
 ];
