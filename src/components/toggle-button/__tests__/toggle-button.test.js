158be00eb84f28f146855d821f91631a27950d71
Alex Pate <alex@kalohq.com>
2018-01-18 11:48:24 +0000

    Import ToggleButton/ToggleInput

---
+++ src/components/toggle-button/__tests__/toggle-button.test.js
@@ -0,0 +1,31 @@
+/* @flow */
+/* eslint-env jest */
+import React from 'react';
+import {shallow} from 'enzyme';
+import serializer from 'enzyme-to-json/serializer';
+
+import ToggleButton from '../toggle-button';
+
+expect.addSnapshotSerializer(serializer);
+
+describe('components/toggle-button', () => {
+  describe('ToggleButton (default)', () => {
+    const create = (spread = {}) => {
+      const props = {
+        // (insert your default props here)
+        ...spread,
+      };
+      const element = <ToggleButton {...props} />;
+      return {props, element};
+    };
+
+    it('should shallow render as expected', () => {
+      const {element} = create({
+        // (insert test specific props here)
+        children: 'Child',
+      });
+      const result = shallow(element);
+      expect(result).toMatchSnapshot();
+    });
+  });
+});
