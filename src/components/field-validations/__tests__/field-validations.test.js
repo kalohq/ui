dada4cbbcbded89b5679ad6fd20a5c38916afe51
Alex Pate <alex@kalohq.com>
2018-01-18 12:59:33 +0000

    Fix validations tests

--- src/components/field-validations/__tests__/field-validations.test.js
+++ src/components/field-validations/__tests__/field-validations.test.js
@@ -1,35 +1,33 @@
 /* @flow */
 /* eslint-env jest */
-import expect from 'expect';
 import {shallow} from 'enzyme';
-import {testComponent} from 'utils/test/react';
+import React from 'react';
+import renderer from 'react-test-renderer';
 
 import FieldValidations from '../field-validations';
 import FieldValidation from '../../field-validation';
 
-describe('components/field-validations', () => {
-  describe('FieldValidations', () => {
-    const create = testComponent(FieldValidations, () => ({
-      validations: [
-        {
-          message: 'Email can’t be empty',
-        },
-        {
-          message: 'Email must be of email type',
-        },
-      ],
-    }));
+describe('FieldValidations', () => {
+  const defaultProps = {
+    validations: [
+      {
+        message: 'Email can’t be empty',
+      },
+      {
+        message: 'Email must be of email type',
+      },
+    ],
+  };
+  const create = props => <FieldValidations {...defaultProps} {...props} />;
 
-    it('should render shallow component ok', () => {
-      const {element} = create();
-      const result = shallow(element);
-      expect(result).toExist('should render OK');
-    });
+  test('should render shallow component ok', () => {
+    const element = renderer.create(<FieldValidations />).toJSON();
+    expect(element).toMatchSnapshot();
+  });
 
-    it('should render two validation components', () => {
-      const {element} = create();
-      const result = shallow(element);
-      expect(result.find(FieldValidation).length).toEqual(2);
-    });
+  it('should render two validation components', () => {
+    const element = create();
+    const result = shallow(element);
+    expect(result.find(FieldValidation).length).toEqual(2);
   });
 });
