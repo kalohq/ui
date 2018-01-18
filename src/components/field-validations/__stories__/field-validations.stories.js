daa95dfda7c74a0706a6988196e1652a5e4c0b8b
Alex Pate <alex@kalohq.com>
2018-01-18 14:59:41 +0000

    Make field validations story iterable

--- src/components/field-validations/__stories__/field-validations.stories.js
+++ src/components/field-validations/__stories__/field-validations.stories.js
@@ -1,15 +1,13 @@
-/* @flow */
 import React from 'react';
-import {List} from 'immutable';
 
 import FieldValidations from '../field-validations';
-
-const validations = List({message: 'Your email is incorrect'});
 
 export const examples = [
   {
     title: 'FieldValidations (default)',
     description: 'An example of FieldValidations (default)',
-    render: () => <FieldValidations validations={validations} />,
+    render: () => (
+      <FieldValidations validations={[{message: 'Your email is incorrect'}]} />
+    ),
   },
 ];
