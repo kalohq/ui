7bf5d02ae6825f18889503ff7032bfbceb8feae1
Alex Pate <alex@kalohq.com>
2018-01-19 11:42:23 +0000

    Add some basic render snapshots for Select

--- src/components/select/__tests__/select.test.js
+++ src/components/select/__tests__/select.test.js
@@ -2,30 +2,86 @@
 /* eslint-env jest */
 import React from 'react';
 import {shallow} from 'enzyme';
-import serializer from 'enzyme-to-json/serializer';
+import renderer from 'react-test-renderer';
+import {random} from 'lodash';
+import {ThemeProvider} from 'emotion-theming';
 
-import Select from '../select';
+import theme from '../../theme';
 
-expect.addSnapshotSerializer(serializer);
+import Select, {Option} from '../';
 
-describe('components/select', () => {
-  describe('Select (default)', () => {
-    const create = (spread = {}) => {
-      const props = {
-        // (insert your default props here)
-        ...spread,
-      };
-      const element = <Select {...props} />;
-      return {props, element};
-    };
+const defaultProps = {};
 
-    it('should shallow render as expected', () => {
-      const {element} = create({
-        // (insert test specific props here)
-        children: 'Child',
-      });
-      const result = shallow(element);
-      expect(result).toMatchSnapshot();
-    });
+const OPTIONS = ['Hertfordshire', 'Bedfordshire', 'Oxfordshire', 'Hampshire'];
+
+describe('Select', () => {
+  const create = props =>
+    shallow(
+      <ThemeProvider theme={theme}>
+        <Select {...defaultProps} {...props}>
+          {OPTIONS.map(option => (
+            <Option key={option} value={option}>
+              {option}
+            </Option>
+          ))}
+        </Select>
+      </ThemeProvider>
+    );
+
+  test('should render shallow component ok', () => {
+    const element = renderer
+      .create(
+        <ThemeProvider theme={theme}>
+          <Select {...defaultProps}>
+            {OPTIONS.map(option => (
+              <Option key={option} value={option}>
+                {option}
+              </Option>
+            ))}
+          </Select>
+        </ThemeProvider>
+      )
+      .toJSON();
+    expect(element).toMatchSnapshot();
+  });
+
+  test('should render a disabled select', () => {
+    const element = renderer
+      .create(
+        <ThemeProvider theme={theme}>
+          <Select {...defaultProps} disabled={true}>
+            {OPTIONS.map(option => (
+              <Option key={option} value={option}>
+                {option}
+              </Option>
+            ))}
+          </Select>
+        </ThemeProvider>
+      )
+      .toJSON();
+    expect(element).toMatchSnapshot();
+  });
+
+  test('should render a readonly select', () => {
+    const element = renderer
+      .create(
+        <ThemeProvider theme={theme}>
+          <Select {...defaultProps} readonly={true}>
+            {OPTIONS.map(option => (
+              <Option key={option} value={option}>
+                {option}
+              </Option>
+            ))}
+          </Select>
+        </ThemeProvider>
+      )
+      .toJSON();
+    expect(element).toMatchSnapshot();
+  });
+
+  test('should pass all unrecognized props on to the root Block', () => {
+    const marginTop = random(10, 20);
+    const element = create({marginTop});
+    expect(element.props().marginTop).toBe(marginTop);
   });
 });
