b6ce31f5d956de02c3c673eeea6f9a3f0a70fc5b
Alex Pate <alex@kalohq.com>
2018-01-22 11:52:25 +0000

    Fix form test

--- src/components/form/__tests__/form.test.js
+++ src/components/form/__tests__/form.test.js
@@ -6,16 +6,14 @@ import {testComponent} from 'utils/test/react';
 
 import Form from '../form';
 
-describe('components/form', () => {
-  describe('Form', () => {
-    const create = testComponent(Form, () => ({
-      actions: [],
-    }));
+describe('Form', () => {
+  const create = testComponent(Form, () => ({
+    actions: [],
+  }));
 
-    it('should render OK', () => {
-      const {element} = create();
-      const result = shallow(element);
-      expect(result).toExist('should render OK');
-    });
+  test('should render OK', () => {
+    const {element} = create();
+    const result = shallow(element);
+    expect(result.exists()).toEqual(true);
   });
 });
