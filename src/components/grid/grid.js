ce60d570ef91c584ef521fbcc6fa517ba252b220
Alex Pate <alex@kalohq.com>
2018-01-31 13:00:55 +0000

    Use padding rather than margin for gutters

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -1,51 +1,50 @@
 /* @flow */
 import React from 'react';
+import styled from 'react-emotion';
 import {toNumber} from 'lodash';
 
 import {Box, Flex} from '../layout';
 
-const GRID_MAX_WIDTH = 1200;
-
 /**
  * Some low level grid components
  */
-type TGridProps = {
-  children?: any,
-};
-
-export function Grid(props: TGridProps) {
-  const {children, ...otherProps} = props;
-
-  return (
-    <Flex
-      width="100%"
-      flexDirection="column"
-      maxWidth={GRID_MAX_WIDTH}
-      margin="0 auto"
-      {...otherProps}
-    >
-      {children}
-    </Flex>
-  );
-}
+export const Grid = styled(Flex)`
+  width: 100%;
+  flex-direction: column;
+  max-width: ${props => props.theme.grid.gridMaxWidth};
+  margin: 0 auto;
+`;
 
 type TRowProps = {
   children?: any,
   gutter?: string,
+  collapse?: boolean,
   spacing?: 'small' | 'medium' | 'large' | 'extra-large',
 };
 
 export function Row(props: TRowProps) {
-  const {children, spacing = 'small', gutter = null, ...otherProps} = props;
+  const {
+    children,
+    spacing = 'small',
+    gutter = null,
+    collapse = false,
+    ...otherProps
+  } = props;
+
   return (
-    <Flex width="100%" marginLeft={gutter} marginRight={gutter} {...otherProps}>
+    <Flex
+      width="100%"
+      paddingLeft={gutter}
+      paddingRight={gutter}
+      {...otherProps}
+    >
       {React.Children.map(
         children,
         child =>
           child
             ? React.cloneElement(child, {
-                paddingLeft: spacing,
-                paddingRight: spacing,
+                paddingLeft: !collapse ? spacing : null,
+                paddingRight: !collapse ? spacing : null,
               })
             : null
       )}
@@ -54,24 +53,18 @@ export function Row(props: TRowProps) {
 }
 
 type TColumnProps = {
-  component?: React$Element<*> | string,
   columns?: number,
   children?: any,
   spacing?: 'small',
 };
 
 export function Column(props: TColumnProps) {
-  const {component, columns = 12, children, ...otherProps} = props;
+  const {columns = 12, children, ...otherProps} = props;
 
   const _width = `${100 / 12 * toNumber(columns)}%`;
 
   return (
-    <Box
-      flexDirection="row"
-      component={component}
-      width={_width}
-      {...otherProps}
-    >
+    <Box flexDirection="row" width={_width} {...otherProps}>
       {children}
     </Box>
   );
