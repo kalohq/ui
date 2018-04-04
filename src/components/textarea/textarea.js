6cec33ae53d2e6404393d86c6d6980685f113c45
Alex Pate <alex@kalohq.com>
2018-04-04 16:24:42 +0100

    Add focus state to textarea

--- src/components/textarea/textarea.js
+++ src/components/textarea/textarea.js
@@ -65,7 +65,9 @@ const StyledTextarea = styled.textarea`
       }
 
       &:not(:read-only):not(:disabled):focus {
+        outline: 0;
         border: ${props.theme.input.inputActiveBorder};
+        box-shadow: 0 0 0 3px rgb(238, 244, 250);
       }
     `};
 
