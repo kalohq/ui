8b9811a7c785687c7021c5728cd709363c67289d
Alex Pate <alex@kalohq.com>
2018-08-08 12:28:28 +0100

    Add build step to compile component CSS files in to single bundle

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const babel = require('gulp-babel');
+const concat = require('gulp-concat');
 
 /**
  * Compile component CSS. We also convert variables here
  * to make the published package more consumable.
  */
-gulp.task('css', () => {
+gulp.task('react:css', () => {
   return gulp
     .src('./src/components/**/*.css')
     .pipe(postcss())
@@ -16,7 +17,7 @@ gulp.task('css', () => {
 /**
  * Compile component JS from ES6/JSX down to ES5
  */
-gulp.task('js', () =>
+gulp.task('react:js', () =>
   gulp
     .src([
       'src/**/*.js',
@@ -37,7 +38,7 @@ gulp.task('js', () =>
 /**
  * Copy component assets (font files, images)
  */
-gulp.task('copy-files', () =>
+gulp.task('react:copy-files', () =>
   gulp
     .src(
       'src/components/**/*(*.woff|*.woff2|*.ttf|*.jpg|*.jpeg|*.png|*.gif|*.svg)'
@@ -48,18 +49,29 @@ gulp.task('copy-files', () =>
 /**
  * Copy design tokens to lib
  */
-gulp.task('copy-design-tokens', () =>
+gulp.task('react:copy-design-tokens', () =>
   gulp
     .src(['src/design-tokens/*(*.js|*.json|*.css)'])
     .pipe(gulp.dest('lib/design-tokens'))
 );
 
 /**
- * General tasks
+ * Bundle CSS. We also convert variables here
+ * to make the published package more consumable.
  */
-gulp.task('build-production', [
-  'css',
-  'js',
-  'copy-files',
-  'copy-design-tokens',
+gulp.task('css:build-css', () => {
+  return gulp
+    .src(['./src/components/**/*.css', '!./src/components/**/*.react.css'])
+    .pipe(concat('ui-bundle.css'))
+    .pipe(postcss())
+    .pipe(gulp.dest('css-package-lib'));
+});
+
+gulp.task('build-react-package', [
+  'react:css',
+  'react:js',
+  'react:copy-files',
+  'react:copy-design-tokens',
 ]);
+
+gulp.task('build-css-package', ['css:build-css']);
