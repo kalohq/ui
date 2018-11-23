b50119a287bee70c9d598befc6e1cd4776369e7c
Alex Pate <alex@kalohq.com>
2018-11-23 10:39:49 +0000

    Move Icon to CSS

---
+++ src/components/icon/__tests__/icon.test.js
@@ -0,0 +1,47 @@
+/* eslint-env jest */
+import * as React from 'react';
+import {mount} from '../../../utils/test/enzyme';
+
+import Icon from '../icon';
+
+describe('Icon', () => {
+  const create = props => <Icon {...props}>{props.children}</Icon>;
+
+  it('should render an icon', () => {
+    const element = create({
+      children: 'add',
+    });
+
+    const result = mount(element);
+    expect(result.exists()).toBe(true);
+  });
+
+  it('should allow a color from the palette to be set', () => {
+    const element = create({
+      color: 'blue500',
+    });
+
+    const result = mount(element);
+    expect(result.render().hasClass('fill-blue500')).toBe(true);
+  });
+
+  it('should allow the size to be changed', () => {
+    const element = create({
+      size: 24,
+    });
+
+    const result = mount(element);
+    expect(result.render().hasClass('ui-icon--size-24')).toBe(true);
+    expect(result.find('svg').prop('width')).toEqual(24);
+    expect(result.find('svg').prop('height')).toEqual(24);
+  });
+
+  it('should set an interactive class if an onClick event is passed through', () => {
+    const element = create({
+      onClick: () => {},
+    });
+
+    const result = mount(element);
+    expect(result.render().hasClass('ui-icon--interactive')).toBe(true);
+  });
+});
--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -1,12 +1,13 @@
 /* @flow */
 import * as React from 'react';
-import styled from 'react-emotion';
-import {Inline} from '../layout';
-import DefaultTheme from '../theme';
+import cx from 'classnames';
+import {UIBase} from '../layout';
 
 import type {ICON_SIZE, ICON_COLOR} from './constants';
-
 import {ICONS, DEFAULT_SIZE, DEFAULT_COLOR} from './constants';
+
+import styles from './icon.css';
+import fills from '../../styles/kalo-ui-fills.css';
 
 type Props = {
   children: $Keys<typeof ICONS>,
@@ -18,16 +19,6 @@ type Props = {
   title?: string,
 };
 
-const StyledIcon = styled(Inline)`
-  user-select: none;
-  line-height: 1em;
-  vertical-align: middle;
-  cursor: ${props => (props.interactive ? 'pointer' : 'inherit')};
-  font-size: ${props => `${props.size}px`};
-  height: ${props => `${props.size}px`};
-  fill: ${props => props.theme.colors[props.color]};
-`;
-
 export default function Icon(props: Props) {
   const {
     children,
@@ -35,27 +26,32 @@ export default function Icon(props: Props) {
     color = DEFAULT_COLOR,
     className,
     onClick,
-    theme = DefaultTheme,
     title,
     ...otherProps
   } = props;
 
+  const _classNames = cx(
+    {
+      [styles['ui-icon']]: true,
+      [styles[`ui-icon--size-${size}`]]: true,
+      [styles['ui-icon--interactive']]: Boolean(onClick),
+      [fills[`fill-${color}`]]: true,
+    },
+    className
+  );
+
   return (
-    <StyledIcon
-      className={className}
+    <UIBase
       component="i"
-      interactive={!!onClick}
       onClick={onClick}
-      theme={theme}
-      size={size}
-      color={color}
       title={title}
+      className={_classNames}
       {...otherProps}
     >
       <svg width={size} height={size} aria-hidden="true">
         {title ? <title>{title}</title> : null}
         <use xlinkHref={`#${children}`} />
       </svg>
-    </StyledIcon>
+    </UIBase>
   );
 }
