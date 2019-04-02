c0099c634a0ee3de97b8ae3d581e9515579a249d
Alex Pate <alex@kalohq.com>
2019-04-02 12:09:47 +0100

    Move ImageInput JS to class

--- src/components/image-input/__examples__/image-input.examples.js
+++ src/components/image-input/__examples__/image-input.examples.js
@@ -1,38 +1,53 @@
 /* @flow */
 import React from 'react';
-// import ImageInput from '../image-input';
+import imageInput from '../image-input';
 
 export const examples = [
   {
     title: 'Image Input',
     description: 'An input for the upload of images',
-    render: () => {},
-    html: () => (
-      <div id="inputWrapper" className="image-input__input-wrapper">
-        <input
-          type="file"
-          name="imageInput"
-          id="imageInput"
-          className="ui-input image-input__input"
-          tabIndex="0"
-        />
-        <div className="image-input__pseudo">Add image</div>
-        <div className="image-input__meta">
-          <label htmlFor="imageTitle" className="image-input__label">
-            Title
-          </label>
+    render: () => <div>empty</div>,
+    html: () => {
+      return (
+        <div
+          id="inputWrapper"
+          className="image-input__input-wrapper"
+          ref={el => {
+            const instance = new imageInput({
+              element: el,
+              inputLabel: 'Custom label',
+              inputPlaceholder: 'Custom Placeholder',
+            });
+
+            return instance;
+          }}
+        >
           <input
-            type="text"
-            name="imageTitle"
-            id="imageTitle"
-            className="ui-input image-input__title"
-            placeholder="e.g. Cover illustration"
+            type="file"
+            name="imageInput"
+            id="imageInput"
+            className="ui-input image-input__input"
+            accept="image/*"
+            tabIndex="0"
           />
-          <button className="image-input__reset" onClick="resetImageInput()">
-            Remove
-          </button>
+          <div className="image-input__pseudo">Add image</div>
+          <div className="image-input__meta">
+            <label htmlFor="imageTitle" className="image-input__label">
+              Title
+            </label>
+            <input
+              type="text"
+              name="imageTitle"
+              id="imageTitle"
+              className="ui-input image-input__title"
+              placeholder="e.g. Cover illustration"
+            />
+            <button className="image-input__reset" id="resetButton">
+              Remove
+            </button>
+          </div>
         </div>
-      </div>
-    ),
+      );
+    },
   },
 ];
--- src/components/image-input/image-input.js
+++ src/components/image-input/image-input.js
@@ -1,5 +1,66 @@
 export default class ImageInput {
-  constructor() {
-    console.log('this', this.element);
+  constructor(attr) {
+    this.element = attr.element;
+    this.inputLabel = attr.inputLabel;
+    this.inputPlaceholder = attr.inputPlaceholder;
+
+    this.resetInput = this.resetInput.bind(this);
+    this.handleFileSelect = this.handleFileSelect.bind(this);
+    this.removeThumbnailsElement = this.removeThumbnailsElement.bind(this);
+
+    if (!window.FileReader) return false;
+
+    if (this.element) {
+      this.inputElement = this.element.querySelector('#imageInput');
+      this.resetElement = this.element.querySelector('#resetButton');
+      this.element.addEventListener('change', this.handleFileSelect, false);
+    }
+
+    if (this.resetElement) {
+      this.resetElement.addEventListener('click', this.resetInput);
+    }
+  }
+
+  handleFileSelect(event) {
+    const files = event.target.files[0];
+
+    this.removeThumbnailsElement();
+
+    const images = [...files].filter(file =>
+      new RegExp('image/?').test(file.type)
+    );
+
+    if (images.length >= 1) {
+      const reader = new FileReader(images[0]);
+
+      reader.onload = (theFile => ev => {
+        if (theFile) {
+          const el = document.createElement('div');
+          el.setAttribute('id', 'thumbnails');
+          el.innerHTML = `<img class="image-input__thumb" src="${
+            ev.target.result
+          }" title="${escape(theFile.name)}" />`;
+          document.getElementById('inputWrapper').insertBefore(el, null);
+          document.getElementsByClassName(
+            'image-input__meta'
+          )[0].style.display = 'block';
+        }
+      })(images[0]);
+
+      reader.readAsDataURL(images[0]);
+    }
+  }
+
+  removeThumbnailsElement() {
+    const thumbnailsElement = this.element.querySelector('#thumbnails');
+    if (thumbnailsElement) {
+      thumbnailsElement.remove();
+    }
+  }
+
+  resetInput() {
+    this.removeThumbnailsElement();
+
+    this.inputElement.value = '';
   }
 }
