86a34ab961c8e5346cc6e5c91498791c3b894672
Alex Pate <alex@kalohq.com>
2019-04-08 15:05:01 +0100

    Update styling of StarRating component

---
+++ src/components/star-rating/__examples__/star-rating.examples.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import StarRating from '../';
+
+export const examples = [
+  {
+    title: 'Star rating',
+    description: '',
+    render: () => <StarRating score={5} />,
+  },
+  {
+    title: 'Star rating with half score',
+    render: () => <StarRating score={3.5} />,
+  },
+];
