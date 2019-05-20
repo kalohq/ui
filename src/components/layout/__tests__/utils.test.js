30788a80e035a792e6f50059aac75611c3a77e42
Luca Lischetti <luca@sirlisko.com>
2019-05-20 11:50:14 +0100

    fixed prefixing of inline styles

---
+++ src/components/layout/__tests__/utils.test.js
@@ -0,0 +1,21 @@
+/* eslint-env jest */
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import {makePrimitive} from '../utils';
+
+describe('makePrimitive', () => {
+  let wrapper;
+  const props = {
+    alignItems: 'center',
+  };
+
+  beforeEach(() => {
+    const NewElement = makePrimitive('FooComponent', 'p', 'ui-foo-component');
+    wrapper = renderer.create(<NewElement {...props} />).toJSON();
+  });
+
+  test('should render correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
