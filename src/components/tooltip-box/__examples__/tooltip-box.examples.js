0d274b5c860d7f6605f191fc23bdd64a6ca8fe0b
Luca Lischetti <luca@sirlisko.com>
2019-05-24 08:57:04 +0100

    added support for show right tooltip

--- src/components/tooltip-box/__examples__/tooltip-box.examples.js
+++ src/components/tooltip-box/__examples__/tooltip-box.examples.js
@@ -15,4 +15,20 @@ export const examples = [
       </div>
     ),
   },
+  {
+    title: 'TooltipBox - show right',
+    render: () => (
+      <TooltipBox text="Hello! I am a tooltip" show="right">
+        Hover me :)
+      </TooltipBox>
+    ),
+    html: () => (
+      <div className="tooltip-container">
+        Hover me :)
+        <div className="tooltip-bubble tooltip-bubble--right">
+          Hello! I am a tooltip
+        </div>
+      </div>
+    ),
+  },
 ];
--- src/components/tooltip-box/__tests__/tooltip-box.test.js
+++ src/components/tooltip-box/__tests__/tooltip-box.test.js
@@ -9,7 +9,17 @@ describe('TooltipBox', () => {
     const wrapper = renderer
       .create(<TooltipBox text="Hello! I am a tooltip">Hover me :)</TooltipBox>)
       .toJSON();
+    expect(wrapper).toMatchSnapshot();
+  });
 
+  test('should render properly - show right', () => {
+    const wrapper = renderer
+      .create(
+        <TooltipBox text="Hello! I am a tooltip" show="right">
+          Hover me :)
+        </TooltipBox>
+      )
+      .toJSON();
     expect(wrapper).toMatchSnapshot();
   });
 });
