7e691aab2c3ee9c3247a40b99121de04a698d1f3
Luca Lischetti <luca@sirlisko.com>
2019-05-24 08:57:04 +0100

    added support for show right tooltip

--- src/components/tooltip-box/tooltip-box.react.js
+++ src/components/tooltip-box/tooltip-box.react.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 import styles from './tooltip-box.css';
 
+const SUPPORTED_DIRECTIONS = ['right'];
+
 const TooltipBox = props => {
-  const {children, text, ...otherProps} = props;
+  const {children, text, show, className, ...otherProps} = props;
+
+  const classnames = {
+    container: cx(styles['tooltip-container'], className),
+    bubble: cx(styles['tooltip-bubble'], {
+      [styles[`tooltip-bubble--${show}`]]: SUPPORTED_DIRECTIONS.includes(show),
+    }),
+  };
 
   return (
-    <div className={styles['tooltip-container']} {...otherProps}>
+    <div className={classnames.container} {...otherProps}>
       {children}
-      <div className={styles['tooltip-bubble']}>{text}</div>
+      <div className={classnames.bubble}>{text}</div>
     </div>
   );
 };
@@ -23,6 +33,10 @@ TooltipBox.propTypes = {
    * It's the element that triggers the visibility of the tooltip.
    */
   children: PropTypes.node.isRequired,
+  /**
+   * the direction of the tooltip ['right'] (if not indicated pops from the bottom).
+   */
+  show: PropTypes.oneOf(['right']),
   className: PropTypes.string,
 };
 
