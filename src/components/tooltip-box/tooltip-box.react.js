1b561a39681d7a62f3a3430726a2ae2ea737c15e
Luca Lischetti <luca@sirlisko.com>
2019-05-28 10:34:11 +0100

    added tooltip type info

--- src/components/tooltip-box/tooltip-box.react.js
+++ src/components/tooltip-box/tooltip-box.react.js
@@ -3,14 +3,26 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 import styles from './tooltip-box.css';
+import Icon from '../icon';
 
 const SUPPORTED_DIRECTIONS = ['right'];
+const TYPE_ICON_MAP = {
+  info: 'info_outline',
+};
 
 const TooltipBox = props => {
-  const {children, text, show, className, ...otherProps} = props;
+  const {children, text, show, className, type, ...otherProps} = props;
+
+  const isTypeSupported = Boolean(TYPE_ICON_MAP[type]);
 
   const classnames = {
-    container: cx(styles['tooltip-container'], className),
+    container: cx(
+      styles['tooltip-container'],
+      {
+        [styles[`tooltip-container--${type}`]]: isTypeSupported,
+      },
+      className
+    ),
     bubble: cx(styles['tooltip-bubble'], {
       [styles[`tooltip-bubble--${show}`]]: SUPPORTED_DIRECTIONS.includes(show),
     }),
@@ -18,7 +30,10 @@ const TooltipBox = props => {
 
   return (
     <div className={classnames.container} {...otherProps}>
-      {children}
+      {children}{' '}
+      {isTypeSupported && (
+        <Icon color="currentColor">{TYPE_ICON_MAP[type]}</Icon>
+      )}
       <div className={classnames.bubble}>{text}</div>
     </div>
   );
@@ -37,6 +52,10 @@ TooltipBox.propTypes = {
    * the direction of the tooltip ['right'] (if not indicated pops from the bottom).
    */
   show: PropTypes.oneOf(['right']),
+  /**
+   * the type of the tooltip box ['info']
+   */
+  type: PropTypes.oneOf(['info']),
   className: PropTypes.string,
 };
 
